9483de9f1dd524b557144105711ef4e2499d3f03
a20rubdelsan <a20rubdelsan@inspedralbes.cat>
2024-02-27 09:53:37 +0100

    v-for

---
+++ web/actions-vue/src/components/Number.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="number in numbers" :key="number" @click="handleClick(number)">
+        {{ number }} - {{ clickedNumber === number ? 'Clicado' : 'No clicado' }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      clickedNumber: null
+    };
+  },
+  methods: {
+    handleClick(number) {
+      this.clickedNumber = number;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos específicos del componente si es necesario */
+</style>
